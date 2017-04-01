62648dcd3d1c7e44675f15b15a5e9863bed1ff3d
Patricio Napoli <patricio.napoli@outlook.com>
2017-04-01 16:40:14 -0300

    Refactoring for bonsai nginx and uwsgi

--- dashboard/service.py
+++ dashboard/service.py
@@ -1,8 +1,16 @@
+import json
 from django.shortcuts import render
+from .models import *
+from django.core.exceptions import PermissionDenied
 
 
 def receive_logs(request):
-    return render(request, "dashboard/receive-logs.html")
+    json_str = '{"app": "TestApp", "key": "sarasacosmica", "http": [{"code": "404", "route": "/firuli", "timestamp": 1}], "logs": [{"method": "","description": "","timestamp": 1}]}'
+    data = json.loads(json_str)
+    app_found = App.objects.filter(name=data['app'], apikey=data['key']).exists()
+    if app_found:
+        return render(request, "dashboard/receive-logs.html")
+    raise PermissionDenied
 
 
 def get_logs(request):
