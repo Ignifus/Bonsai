fbf3e107cf55d760d1f4e92d981cd13f088098db
Patricio Napoli <patricio.napoli@outlook.com>
2017-04-01 12:24:04 -0300

    Refactoring for bonsai nginx and uwsgi

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'bio'
 urlpatterns = [
     url(r'^$', views.landing, name='landing'),
-    url(r'^login_auth/$', views.login_auth, name='home'),
+    url(r'^login_auth/$', views.login_auth, name='login_auth'),
     url(r'^logout_auth/$', views.logout_auth, name='logout_auth'),
     url(r'^home/$', views.home, name='home'),
     url(r'^server/$', views.server, name='server'),
