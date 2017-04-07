e581948eba07ddc78aaae4c20182ad0f733fa6ba
Patricio Napoli <patricio.napoli@outlook.com>
2017-04-07 20:01:33 -0300

    Refactoring for bonsai nginx and uwsgi

--- static/dashboard/js/charts.js
+++ static/dashboard/js/charts.js
@@ -1,22 +1,62 @@
-$(function(){
-    refresh();
-});
-
 function refresh(){
-    var chartData = {};
     $.ajax({
         url: '/get-server-logs',
         dataType: 'application/json',
         complete: function(data){
-            chartData = JSON.parse(data.responseText)[0].fields;
+
+            var obj = JSON.parse(data.responseText)[0].fields;
 
             var date = new Date(0);
-            date.setUTCSeconds(chartData.timestamp);
+            date.setUTCSeconds(obj.timestamp);
 
             var ss = date.getSeconds();
             var min = date.getMinutes();
             var hh = date.getHours();
+
+            obj.timestamp = hh+":"+min+":"+ss;
+
+            chart.dataProvider.push(obj);
+            chart.validateData();
         }
     });
-    setTimeout(refresh,5000);
 }
+
+var chart = AmCharts.makeChart( "chartcpu", {
+    "type": "serial",
+    "theme": "light",
+    "zoomOutButton": {
+        "backgroundColor": '#000000',
+        "backgroundAlpha": 0.15
+    },
+    "dataProvider": [],
+    "categoryField": "timestamp",
+    "categoryAxis": {
+        "parseDates": false,
+        "minPeriod": "ss",
+        "dashLength": 1,
+        "gridAlpha": 0.15,
+        "axisColor": "#DADADA"
+    },
+    "graphs": [ {
+        "id": "g1",
+        "valueField": "cpu_usage",
+        "bullet": "round",
+        "bulletBorderColor": "#FFFFFF",
+        "bulletBorderThickness": 2,
+        "lineThickness": 2,
+        "lineColor": "#b5030d",
+        "negativeLineColor": "#0352b5",
+        "hideBulletsCount": 50
+    } ],
+    "chartCursor": {
+        "cursorPosition": "mouse"
+    },
+    "chartScrollbar": {
+        "graph": "g1",
+        "scrollbarHeight": 40,
+        "color": "#FFFFFF",
+        "autoGridCount": true
+    }
+} );
+
+setInterval(refresh, 1000);
