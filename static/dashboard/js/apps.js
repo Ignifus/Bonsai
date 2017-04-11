bfadfcbfd916350ebd157f99ac98c13a6075a850
Patricio Napoli <patricio.napoli@outlook.com>
2017-04-10 23:09:30 -0300

    Refactoring for bonsai nginx and uwsgi

--- static/dashboard/js/apps.js
+++ static/dashboard/js/apps.js
@@ -5,14 +5,27 @@ function queryLogs(){
         dataType: 'json',
         method: 'POST',
         data: {
-          "app": "TestAppA", "from": "20"
+            "app": "TestAppA", "from": "15"
         },
         success: function(data){
-            var obj = JSON.parse(data.responseText)[0].fields;
+            if(data == null || data.length === 0) return;
+
+            data.forEach(function(item){
+                if(item.fields.code) $('.http').append("<p>" + renderHttp(item.fields) + "</p>");
+                else $('.log').append("<p>" + renderLog(item.fields) + "</p>");
+            });
 
             $('.i-content').html('<pre>'+ obj.method +'</pre>')
         }
     });
 }
 
-setInterval(queryLogs, 2000);
+function renderHttp(item) {
+    return "timestamp: " + item.timestamp  + ", method: " + item.code + " , app: " + item.app;
+}
+
+function renderLog(item) {
+    return "timestamp: " + item.timestamp  + ", method: " + item.method + ", description: " + item.description + " , app: " + item.app;
+}
+
+setInterval(queryLogs, 15000);
