5e049bd04ef7af5a88658e7dc3b373d9056a482d
Patricio Napoli <master@ignifus.com>
2017-06-03 21:17:28 -0300

    CI Tests

--- bonsai/settings.py
+++ bonsai/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Bonsai',
         'USER': 'bonsai',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
